c784e9682d1c5ab0ed7b2f0f1ab92e4d264ac9cb
Drew Zemke <drew.zemke@gmail.com>
2023-10-09 10:19:04 -0700

    feat: parse "tomorrow" and "tom"

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@ pub fn parse(text: &str, now: &DateTime<Local>) -> Option<DateTime<Local>> {
                 .and_local_timezone(Local)
                 .unwrap(),
         )
+    } else if text == "tomorrow" || text == "tom" {
+        Some(
+            now.date_naive()
+                .checked_add_days(Days::new(1))
+                .unwrap()
+                .and_time(NaiveTime::default())
+                .and_local_timezone(Local)
+                .unwrap(),
+        )
     } else {
         None
     }
@@ -29,14 +38,29 @@ mod tests {
     #[test]
     fn parse_today() {
         let now = parse_date_time("2023-10-08 20:29");
-        let result1 = parse("today", &now).unwrap();
-        assert_eq!(result1.month(), 10);
-        assert_eq!(result1.day(), 8);
-        assert_eq!(result1.year(), 2023);
+        let result = parse("today", &now).unwrap();
+        assert_eq!(result.month(), 10);
+        assert_eq!(result.day(), 8);
+        assert_eq!(result.year(), 2023);
 
-        let result2 = parse("tod", &now).unwrap();
-        assert_eq!(result2.month(), 10);
-        assert_eq!(result2.day(), 8);
-        assert_eq!(result2.year(), 2023);
+        let result = parse("tod", &now).unwrap();
+        assert_eq!(result.month(), 10);
+        assert_eq!(result.day(), 8);
+        assert_eq!(result.year(), 2023);
+    }
+
+    #[test]
+    fn parse_tomorrow() {
+        let now = parse_date_time("2023-10-08 20:29");
+        let result = parse("tomorrow", &now).unwrap();
+        assert_eq!(result.month(), 10);
+        assert_eq!(result.day(), 9);
+        assert_eq!(result.year(), 2023);
+
+        let now = parse_date_time("2023-09-30 20:29");
+        let result = parse("tom", &now).unwrap();
+        assert_eq!(result.month(), 10);
+        assert_eq!(result.day(), 1);
+        assert_eq!(result.year(), 2023);
     }
 }
