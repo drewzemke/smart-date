91335a0a0d3063b6c58e8b319618ef1d63ee6343
Drew Zemke <drew.zemke@gmail.com>
2023-10-17 08:03:37 -0700

    refactor: move `Parsed` type back into `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
 #![warn(clippy::all, clippy::pedantic, clippy::unwrap_used)]
 use chrono::{Days, NaiveDate};
-use parsed::Parsed;
+use std::ops::Range;
 
-mod parsed;
+mod parser;
+
+/// Represents some data that has been parsed out of a string.
+/// Contains the data that was extracted as well as the location in
+/// the input string of the substring that was related to the data.
+pub struct Parsed<T> {
+    pub data: T,
+
+    // TODO: consider storing a substring instead, then provide a method to
+    // compute the offset.
+    // see https://stackoverflow.com/questions/67148359/check-if-a-str-is-a-sub-slice-of-another-str
+    pub range: Range<usize>,
+}
+
+impl<T> Parsed<T> {
+    pub fn map<U, F>(self, f: F) -> Parsed<U>
+    where
+        F: FnOnce(T) -> U,
+    {
+        Parsed {
+            data: f(self.data),
+            range: self.range,
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum FlexibleDate {
